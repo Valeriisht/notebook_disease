b2f80e4e6c3b28c8dd767d12799fd5a5fe8981eb
Valeria <valeria@MacBook-Air-Valeria.local>
2025-12-08 12:37:58 +0300

    Add files

---
+++ model_configs.py
@@ -0,0 +1,82 @@
+# For each model
+MODEL_CONFIGS = {
+    "CatBoost": {
+        "params": {
+            "iterations": 1000,
+            "learning_rate": 0.2,
+            "early_stopping_rounds": 10,
+            "eval_metric": "MultiClass",
+            "verbose": 0,
+            "random_state": 42,
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": True,
+        "needs_imputation": False,
+        "shap" : True,
+    },
+    "XGBoost": {
+        "params": {
+            "n_estimators": 1000,
+            "learning_rate": 0.2,
+            "early_stopping_rounds": 20,
+            "eval_metric": "mlogloss",
+            "random_state": 42,
+            "enable_categorical": True,
+            "verbose": False, 
+            "objective":'multi:softprob',
+
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": True,
+        "needs_imputation": False,
+        "shap" : True, 
+    },
+
+    "LightGBM": {
+        "params": {
+            "n_estimators": 1000,
+            "learning_rate": 0.2,
+            "early_stopping_round": 10,
+            "random_state": 42,
+            "verbose": -1
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": True,
+        "needs_imputation": False, 
+        "shap" : True   
+    },
+    "SVM": {
+        "params": {
+            "probability": True,
+            "random_state": 42
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": False,
+        "needs_imputation": True,
+        "shap" : False   
+    },
+    "RandomForest": {
+        "params": {
+            "random_state": 42
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": False,
+        "needs_imputation": True, 
+        "shap" : False     
+    },
+    "LogisticRegression": {
+        "params": {
+            "random_state": 42
+        },
+        "needs_cat_features": False,
+        "use_onehot": True,
+        "use_eval_set": False,
+        "needs_imputation": True, 
+        "shap" : False     
+    }
+}
